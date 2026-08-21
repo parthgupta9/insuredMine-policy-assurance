37bc7913b673029c06464711ce8324459da50bbd
Parth Gupta <parth615gupta@gmail.com>
2026-08-21 11:52:02 +0530

    csv parsing

--- src/workers/uploadWorkers.js
+++ src/workers/uploadWorkers.js
@@ -1,9 +1,27 @@
 const { Worker } = require("worker_threads");
 const { workerData, parentPort } =
   require("worker_threads");
+const fs = require("fs");
+const csv = require("csv-parser");
 
-console.log(workerData);
-parentPort.postMessage({
-  success: true,
-  message: "Worker thread executed successfully",
-});
+const rows = [];
+
+fs.createReadStream(workerData.filePath)
+  .pipe(csv())
+  .on("data", (row) => {
+    rows.push(row);
+  })
+  .on("end", () => {
+    console.log(`CSV file ${workerData.fileName} processed successfully`);
+    parentPort.postMessage({
+      success: true,
+      totalRows: rows.length,
+      sampleRow: rows[0],
+    });
+  })
+  .on("error", (error) => {
+    parentPort.postMessage({
+      success: false,
+      message: error.message,
+    });
+  });
