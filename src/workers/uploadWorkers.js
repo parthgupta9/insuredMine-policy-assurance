57fb5547747fc4cecd53fb84e1b938c883da5c37
Parth Gupta <parth615gupta@gmail.com>
2026-08-21 12:05:05 +0530

    csv parsing updated

--- src/workers/uploadWorkers.js
+++ src/workers/uploadWorkers.js
@@ -1,4 +1,6 @@
-const { Worker } = require("worker_threads");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Agent = require("../models/Agent");
 const { workerData, parentPort } =
   require("worker_threads");
 const fs = require("fs");
@@ -6,19 +8,85 @@ const csv = require("csv-parser");
 
 const rows = [];
 
+async function processAgents(agentMap) {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  const agents = [...agentMap.keys()].map(
+    (agentName) => ({
+      agentName,
+    })
+  );
+
+  const insertedAgents =
+    await Agent.insertMany(agents, {
+      ordered: false,
+    });
+
+  return insertedAgents.length;
+}
+
 fs.createReadStream(workerData.filePath)
   .pipe(csv())
   .on("data", (row) => {
     rows.push(row);
   })
-  .on("end", () => {
-    console.log(`CSV file ${workerData.fileName} processed successfully`);
-    parentPort.postMessage({
-      success: true,
-      totalRows: rows.length,
-      sampleRow: rows[0],
-    });
-  })
+ .on("end",async () => {
+  const agentMap = new Map();
+  const carrierMap = new Map();
+  const lobMap = new Map();
+  const accountMap = new Map();
+  const userMap = new Map();
+
+  rows.forEach((row) => {
+    if (row.agent) {
+      agentMap.set(row.agent.trim(), null);
+    }
+
+    if (row.company_name) {
+      carrierMap.set(
+        row.company_name.trim(),
+        null
+      );
+    }
+
+    if (row.category_name) {
+      lobMap.set(
+        row.category_name.trim(),
+        null
+      );
+    }
+
+    if (row.account_name) {
+      accountMap.set(
+        row.account_name.trim(),
+        null
+      );
+    }
+
+    const userKey =
+      `${row.email?.trim()}_${row.phone?.trim()}`;
+
+    userMap.set(userKey, null);
+  });
+    console.log([...agentMap.keys()]);
+
+  try {
+  const insertedAgents =
+    await processAgents(agentMap);
+
+  parentPort.postMessage({
+    success: true,
+    totalRows: rows.length,
+    insertedAgents,
+    uniqueAgents: agentMap.size,
+  });
+} catch (error) {
+  parentPort.postMessage({
+    success: false,
+    message: error.message,
+  });
+}
+})
   .on("error", (error) => {
     parentPort.postMessage({
       success: false,
